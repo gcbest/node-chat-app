1a8ada88959063006ce7c337dd4af176c11e42ab
Godfrey Best <gcb246@stern.nyu.edu>
2017-05-17 15:05:24 -0400

    styling added

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ io.on('connection', (socket) => {
 	socket.on('createMessage', (message, callback) => {
 		console.log(message);
 		io.emit('newMessage', generateMessage(message.from, message.text));
-		callback('This is your acknowledgment message');
+		callback();
 	});
 
 	socket.on('createLocationMessage', (coords) => {
