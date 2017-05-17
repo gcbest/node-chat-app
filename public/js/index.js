3e473d6d06c2a8a3343689cba6324ef2519081ee
Godfrey Best <gcb246@stern.nyu.edu>
2017-05-16 23:37:59 -0400

    broadcasting added

--- public/js/index.js
+++ public/js/index.js
@@ -3,22 +3,12 @@ var socket = io();
 
 socket.on('connect', function () {
 	console.log('Connected to server');
-
-	socket.emit('createMessage', {
-		from: 'pete',
-		text: 'my dragon puffs'
-	});
-
 });	
 
 // whenever the connection drops
 socket.on('disconnect', function () {
 	console.log('Disconnected from server');
 });	
-
-socket.on('newEmail', function(email) {
-	console.log('New email', email);
-});
 
 socket.on('newMessage', function(message) {
 	console.log(message);
--- server/server.js
+++ server/server.js
@@ -13,18 +13,39 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-// event handler, socket refers to the individual socket
+// event listener, socket refers to the individual socket
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
+	// socket.emit emits an event to a single connection
+	// io.emit emits an event to every connection
+
 	socket.emit('newMessage', {
-		from: "joe",
-		text: 'hiyo',
-		createdAt: 'today'
+		from: 'Admin',
+		text: 'Welcome to the chat app',
+		createdAt: new Date().getTime()
 	});
+
+	socket.broadcast.emit('newMessage', {
+		from: 'Admin',
+		text: 'New User Joined',
+		createdAt: new Date().getTime()
+	})
 
 	socket.on('createMessage', (message) => {
 		console.log(message);
+		io.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createdAt: new Date().getTime()
+		});
+
+		// broadcast emits a message to everyone except the person who created the message
+		socket.broadcast.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createdAt: new Date().getTime()
+		});
 	});
 
 	socket.on('disconnect', () => {
